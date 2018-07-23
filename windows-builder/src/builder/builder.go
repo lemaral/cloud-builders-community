d3b143c3f396939d791c3369dcb632de13317bbf
Nick Franklin <n.o.franklin@gmail.com>
2018-07-22 20:25:42 -0700

    Better OO design

--- windows-builder/src/builder/builder.go
+++ windows-builder/src/builder/builder.go
@@ -4,13 +4,6 @@ import (
 	"archive/zip"
 	"bytes"
 	"context"
-	"crypto/rand"
-	"crypto/rsa"
-	"crypto/sha1"
-	"encoding/base64"
-	"encoding/binary"
-	"encoding/json"
-	"errors"
 	"fmt"
 	"io"
 	"io/ioutil"
@@ -20,262 +13,76 @@ import (
 	"strings"
 	"time"
 
-	"github.com/masterzen/winrm"
-
 	"cloud.google.com/go/storage"
-	"golang.org/x/oauth2/google"
-	compute "google.golang.org/api/compute/v1"
+	"github.com/masterzen/winrm"
 )
 
 const (
-	prefix         = "https://www.googleapis.com/compute/v1/projects/"
-	imageURL       = prefix + "windows-cloud/global/images/windows-server-1709-dc-core-for-containers-v20180508"
-	zone           = "us-central1-f"
-	instanceName   = "windows-builder"
-	computeTimeout = 120 * time.Second
+	prefix    = "https://www.googleapis.com/compute/v1/projects/"
+	imageURL  = prefix + "windows-cloud/global/images/windows-server-1709-dc-core-for-containers-v20180508"
+	winrmport = 5986
 )
 
-//GCEService returns a Compute Engine service.
-func GCEService(ctx context.Context) (*compute.Service, error) {
-	client, err := google.DefaultClient(ctx, compute.ComputeScope)
-	if err != nil {
-		log.Printf("Failed to create Google Default Client: %v", err)
-		return nil, err
-	}
-	service, err := compute.New(client)
-	if err != nil {
-		log.Printf("Failed to create Compute Service: %v", err)
-		return nil, err
-	}
-
-	return service, nil
+//Server represents a remote Windows server.
+type Server struct {
+	Hostname string
+	Username string
+	Password string
+	Client   Clientlike
 }
 
-//StartWindowsVM starts a Windows VM on GCE and returns host, username, password.
-func StartWindowsVM(ctx context.Context, service *compute.Service, projectID string) (*compute.Instance, error) {
-	startupCmd := `winrm set winrm/config/Service/Auth @{Basic="true”} & winrm set winrm/config/Service @{AllowUnencrypted="true”}`
-	instance := &compute.Instance{
-		Name:        instanceName,
-		MachineType: prefix + projectID + "/zones/" + zone + "/machineTypes/n1-standard-1",
-		Disks: []*compute.AttachedDisk{
-			{
-				AutoDelete: true,
-				Boot:       true,
-				Type:       "PERSISTENT",
-				InitializeParams: &compute.AttachedDiskInitializeParams{
-					DiskName:    "windows-pd",
-					SourceImage: imageURL,
-				},
-			},
-		},
-		Metadata: &compute.Metadata{
-			Items: []*compute.MetadataItems{
-				&compute.MetadataItems{
-					Key:   "windows-startup-script-bat",
-					Value: &startupCmd,
-				},
-			},
-		},
-		NetworkInterfaces: []*compute.NetworkInterface{
-			&compute.NetworkInterface{
-				AccessConfigs: []*compute.AccessConfig{
-					&compute.AccessConfig{
-						Type: "ONE_TO_ONE_NAT",
-						Name: "External NAT",
-					},
-				},
-				Network: prefix + projectID + "/global/networks/default",
-			},
-		},
-		ServiceAccounts: []*compute.ServiceAccount{
-			{
-				Email: "default",
-				Scopes: []string{
-					compute.DevstorageFullControlScope,
-					compute.ComputeScope,
-				},
-			},
-		},
-	}
-
-	op, err := service.Instances.Insert(projectID, zone, instance).Do()
-	if err != nil {
-		log.Printf("GCE Instances insert call failed: %v", err)
-		return nil, err
-	}
-	etag := op.Header.Get("Etag")
-	inst, err := service.Instances.Get(projectID, zone, instanceName).IfNoneMatch(etag).Do()
-	if err != nil {
-		log.Printf("Could not get GCE Instance details after creation: %v", err)
-		return nil, err
-	}
-	log.Printf("Successfully created instance %#v", inst.Name)
-
-	return inst, nil
+//Clientlike allows mocking of winrm.Client for test purposes.
+type Clientlike interface {
+	Run(string, io.Writer, io.Writer) (int, error)
 }
 
-//RefreshWindowsVM refreshes latest info from GCE on a VM.
-func RefreshWindowsVM(ctx context.Context, service *compute.Service, projectID string) (*compute.Instance, error) {
-	inst, err := service.Instances.Get(projectID, zone, instanceName).Do()
-	if err != nil {
-		log.Printf("Could not refresh instance: %v", err)
-		return nil, err
-	}
-	return inst, nil
-}
-
-//StopWindowsVM stops a Windows VM on GCE.
-func StopWindowsVM(ctx context.Context, service *compute.Service, projectID string) error {
-	_, err := service.Instances.Delete(projectID, zone, instanceName).Do()
-	if err != nil {
-		log.Printf("Could not delete instance: %v", err)
-		return err
-	}
-	return nil
-}
-
-//SetFirewallRule allows ingress on WinRM port.
-func SetFirewallRule(ctx context.Context, service *compute.Service, projectID string) error {
-	firewallRule := &compute.Firewall{
-		Allowed: []*compute.FirewallAllowed{
-			&compute.FirewallAllowed{
-				IPProtocol: "tcp",
-				Ports:      []string{"5986"},
-			},
-		},
-		Direction:    "INGRESS",
-		Name:         "allow-winrm-ingress",
-		SourceRanges: []string{"0.0.0.0/0"},
-	}
-	_, err := service.Firewalls.Insert(projectID, firewallRule).Do()
-	if err != nil {
-		log.Printf("Error setting firewall rule: %v", err)
-		return err
-	}
-	return nil
-}
-
-//WindowsPasswordConfig stores metadata to be sent to GCE.
-type WindowsPasswordConfig struct {
-	key      *rsa.PrivateKey
-	password string
-	UserName string    `json:"userName"`
-	Modulus  string    `json:"modulus"`
-	Exponent string    `json:"exponent"`
-	Email    string    `json:"email"`
-	ExpireOn time.Time `json:"expireOn"`
-}
-
-//WindowsPasswordResponse stores data received from GCE.
-type WindowsPasswordResponse struct {
-	UserName          string `json:"userName"`
-	PasswordFound     bool   `json:"passwordFound"`
-	EncryptedPassword string `json:"encryptedPassword"`
-	Modulus           string `json:"modulus"`
-	Exponent          string `json:"exponent"`
-	ErrorMessage      string `json:"errorMessage"`
-}
-
-//ResetWindowsPassword securely resets the admin Windows password.
-//See https://cloud.google.com/compute/docs/instances/windows/automate-pw-generation
-func ResetWindowsPassword(projectID string, service *compute.Service, inst *compute.Instance, username string) (string, error) {
-	//Create random key and encode
-	key, err := rsa.GenerateKey(rand.Reader, 2048)
-	if err != nil {
-		log.Printf("Failed to generate random RSA key: %v", err)
-		return "", err
-	}
-	buf := make([]byte, 4)
-	binary.BigEndian.PutUint32(buf, uint32(key.E))
-	wpc := WindowsPasswordConfig{
-		key:      key,
-		UserName: username,
-		Modulus:  base64.StdEncoding.EncodeToString(key.N.Bytes()),
-		Exponent: base64.StdEncoding.EncodeToString(buf[1:]),
-		Email:    "nobody@nowhere.com",
-		ExpireOn: time.Now().Add(time.Minute * 5),
-	}
-	data, err := json.Marshal(wpc)
-	dstring := string(data)
-	if err != nil {
-		log.Printf("Failed to marshal JSON: %v", err)
-		return "", err
-	}
-
-	//Write key to instance metadata and wait for op to complete
-	log.Print("Writing Windows instance metadata for password reset.")
-	inst.Metadata.Items = append(inst.Metadata.Items, &compute.MetadataItems{
-		Key:   "windows-keys",
-		Value: &dstring,
-	})
-	op, err := service.Instances.SetMetadata(projectID, zone, instanceName, &compute.Metadata{
-		Fingerprint: inst.Metadata.Fingerprint,
-		Items:       inst.Metadata.Items,
-	}).Do()
-	if err != nil {
-		log.Printf("Failed to set instance metadata: %v", err)
-		return "", err
-	}
-	err = WaitForComputeOperation(service, projectID, zone, op)
-	if err != nil {
-		log.Printf("Compute operation timed out")
-		return "", err
-	}
-
-	//Read and decode password
-	log.Print("Waiting for Windows password response.")
-	timeout := time.Now().Add(time.Minute * 3)
-	hash := sha1.New()
-	random := rand.Reader
-	for time.Now().Before(timeout) {
-		output, err := service.Instances.GetSerialPortOutput(projectID, zone, instanceName).Port(4).Do()
+func (s *Server) getClient() Clientlike {
+	if s.Client == nil {
+		endpoint := winrm.NewEndpoint(s.Hostname, winrmport, true, true, nil, nil, nil, 0)
+		client, err := winrm.NewClient(endpoint, s.Username, s.Password)
 		if err != nil {
-			log.Printf("Unable to get serial port output: %v", err)
-			return "", err
-		}
-		responses := strings.Split(output.Contents, "\n")
-		for _, response := range responses {
-			var wpr WindowsPasswordResponse
-			if err := json.Unmarshal([]byte(response), &wpr); err != nil {
-				continue
-			}
-			if wpr.Modulus == wpc.Modulus {
-				decodedPassword, err := base64.StdEncoding.DecodeString(wpr.EncryptedPassword)
-				if err != nil {
-					log.Printf("Cannot Base64 decode password: %v", err)
-					return "", err
-				}
-				password, err := rsa.DecryptOAEP(hash, random, wpc.key, decodedPassword, nil)
-				if err != nil {
-					log.Printf("Cannot decrypt password response: %v", err)
-					return "", err
-				}
-				return string(password), nil
-			}
-		}
-		time.Sleep(2 * time.Second)
-	}
-	err = errors.New("Could not retrieve password before timeout")
-	return "", err
-}
-
-//WaitForComputeOperation waits for a compute operation
-func WaitForComputeOperation(service *compute.Service, projectID string, zone string, op *compute.Operation) error {
-	log.Printf("Waiting for operation %+v to complete", op.Name)
-	timeout := time.Now().Add(computeTimeout)
-	for time.Now().Before(timeout) {
-		newop, err := service.ZoneOperations.Get(projectID, zone, op.Name).Do()
-		if err != nil {
-			log.Printf("Failed to update operation status: %v", err)
-			return err
-		}
-		if newop.Status == "DONE" {
+			log.Printf("Error opening client connection to Windows host: %v", err)
 			return nil
 		}
-		time.Sleep(1 * time.Second)
+		s.Client = client
 	}
-	err := fmt.Errorf("Compute operation %s timed out", op.Name)
-	return err
+	return s.Client
+}
+
+//RunRemoteCommand runs a command on a Windows server.
+func (s *Server) RunRemoteCommand(stdout io.Writer, cmd string) error {
+	var stderr bytes.Buffer
+	client := s.getClient()
+	exit, err := client.Run(cmd, stdout, &stderr)
+	if err != nil {
+		log.Printf("Client experienced error running command: %+v", err)
+		return err
+	}
+	if exit != 0 {
+		log.Printf("Client received non-zero error code %d, stderr %s", exit, stderr.String())
+	}
+	return nil
+}
+
+//PullContainer configures GCR and pulls container.
+func (s *Server) PullContainer(name string) error {
+	cmd := "gcloud --quiet auth configure-docker"
+	out := bytes.NewBuffer([]byte{})
+	err := s.RunRemoteCommand(out, cmd)
+	if err != nil {
+		log.Printf("Unable to configure gcloud as Docker credential helper: %s", out)
+		return err
+	}
+	log.Println(out.String())
+	cmd = fmt.Sprintf("docker pull %s", name)
+	out = bytes.NewBuffer([]byte{})
+	err = s.RunRemoteCommand(out, cmd)
+	if err != nil {
+		log.Printf("Unable to pull container: %s", out)
+		return err
+	}
+	log.Println(out.String())
+	return nil
 }
 
 //NewGCSClient creates a new GCS client.
@@ -380,59 +187,8 @@ func ZipUploadLinux(ctx context.Context, client *storage.Client, projectID strin
 	return bucketname, filename, nil
 }
 
-//Clientlike allows mocking of winrm.Client for test purposes.
-type Clientlike interface {
-	Run(string, io.Writer, io.Writer) (int, error)
-}
-
-//OpenWindowsClient opens a connection to the Windows host.
-func OpenWindowsClient(host string, port int, user string, pass string) (*winrm.Client, error) {
-	endpoint := winrm.NewEndpoint(host, port, true, true, nil, nil, nil, 0)
-	client, err := winrm.NewClient(endpoint, user, pass)
-	if err != nil {
-		log.Printf("Error opening client connection to Windows host: %v", err)
-		return nil, err
-	}
-	return client, nil
-}
-
-//RunRemoteCommand runs a command on a Windows server.
-func RunRemoteCommand(client Clientlike, stdout io.Writer, cmd string) error {
-	var stderr bytes.Buffer
-	exit, err := client.Run(cmd, stdout, &stderr)
-	if err != nil {
-		log.Printf("Client experienced error running command: %+v", err)
-		return err
-	}
-	if exit != 0 {
-		log.Printf("Client received non-zero error code %d, stderr %s", exit, stderr.String())
-	}
-	return nil
-}
-
-//PullContainer configures GCR and pulls container.
-func PullContainer(client Clientlike, name string) error {
-	cmd := "gcloud --quiet auth configure-docker"
-	out := bytes.NewBuffer([]byte{})
-	err := RunRemoteCommand(client, out, cmd)
-	if err != nil {
-		log.Printf("Unable to configure gcloud as Docker credential helper: %s", out)
-		return err
-	}
-	log.Println(out.String())
-	cmd = fmt.Sprintf("docker pull %s", name)
-	out = bytes.NewBuffer([]byte{})
-	err = RunRemoteCommand(client, out, cmd)
-	if err != nil {
-		log.Printf("Unable to pull container: %s", out)
-		return err
-	}
-	log.Println(out.String())
-	return nil
-}
-
 //DownloadUnzipWindows downloads workspace from GCS and unzips.
-func DownloadUnzipWindows(client Clientlike, bucketname string, filename string) error {
+func (s *Server) DownloadUnzipWindows(bucketname string, filename string) error {
 	//Strip directories from filename
 	var stripped string
 	fparts := strings.Split(filename, "/")
@@ -444,7 +200,7 @@ func DownloadUnzipWindows(client Clientlike, bucketname string, filename string)
 	}
 	cmd := fmt.Sprintf("gsutil cp gs://%s/%s C:\\%s", bucketname, filename, stripped)
 	out := bytes.NewBuffer([]byte{})
-	err := RunRemoteCommand(client, out, cmd)
+	err := s.RunRemoteCommand(out, cmd)
 	if err != nil {
 		log.Printf("Unable to download workspace zip from GCS: %s", out)
 		return err
@@ -453,7 +209,7 @@ func DownloadUnzipWindows(client Clientlike, bucketname string, filename string)
 	//TODO: Check this gives the correct behavior on Windows
 	cmd = fmt.Sprintf("cd C:\\ & unzip C:\\%s C:\\workspace", stripped)
 	out = bytes.NewBuffer([]byte{})
-	err = RunRemoteCommand(client, out, cmd)
+	err = s.RunRemoteCommand(out, cmd)
 	if err != nil {
 		log.Printf("Unable to unzip on remote Windows machine: %s", out)
 		return err
@@ -463,11 +219,11 @@ func DownloadUnzipWindows(client Clientlike, bucketname string, filename string)
 }
 
 //RunContainer starts the container on the Windows server.
-func RunContainer(client Clientlike, name string) error {
+func (s *Server) RunContainer(name string) error {
 	//TODO: Check this is the correct Docker mount command.
 	cmd := fmt.Sprintf("docker run -it --mount C:\\workspace %s", name)
 	out := os.Stdout //redirect output straight to tty
-	err := RunRemoteCommand(client, out, cmd)
+	err := s.RunRemoteCommand(out, cmd)
 	if err != nil {
 		log.Printf("Unable to run container remotely: %+v", err)
 		return err
@@ -476,7 +232,7 @@ func RunContainer(client Clientlike, name string) error {
 }
 
 //ZipUploadWindows zips the workspace and uploads back to GCS, overwriting the previous one.
-func ZipUploadWindows(client Clientlike, bucketname string, filename string) error {
+func (s *Server) ZipUploadWindows(bucketname string, filename string) error {
 	//Strip directories from filename
 	var stripped string
 	fparts := strings.Split(filename, "/")
@@ -489,7 +245,7 @@ func ZipUploadWindows(client Clientlike, bucketname string, filename string) err
 	//TODO: Check this gives the correct behavior on Windows
 	cmd := fmt.Sprintf("cd C:\\ & zip C:\\%s C:\\workspace", stripped)
 	out := bytes.NewBuffer([]byte{})
-	err := RunRemoteCommand(client, out, cmd)
+	err := s.RunRemoteCommand(out, cmd)
 	if err != nil {
 		log.Printf("Unable to zip on remote Windows machine: %s", out)
 		return err
@@ -498,7 +254,7 @@ func ZipUploadWindows(client Clientlike, bucketname string, filename string) err
 
 	cmd = fmt.Sprintf("gsutil cp C:\\%s gs://%s/%s", stripped, bucketname, filename)
 	out = bytes.NewBuffer([]byte{})
-	err = RunRemoteCommand(client, out, cmd)
+	err = s.RunRemoteCommand(out, cmd)
 	if err != nil {
 		log.Printf("Unable to upload workspace zip to GCS: %s", out)
 		return err
--- windows-builder/src/main.go
+++ windows-builder/src/main.go
@@ -6,49 +6,29 @@ import (
 	"os"
 
 	"./builder"
-	compute "google.golang.org/api/compute/v1"
-)
-
-var (
-	inst      *compute.Instance
-	host      string
-	username  string
-	password  string
-	container string
-	args      string
-	projectID string
 )
 
 func main() {
 	log.Printf("Starting Windows builder")
 	ctx := context.Background()
+	var server builder.Server
 
 	//Parse environment variables.
-	host = os.Getenv("HOST")
-	username = os.Getenv("USERNAME")
-	password = os.Getenv("PASSWORD")
-	container = os.Getenv("NAME")
-	args = os.Getenv("ARGS")
-	projectID = os.Getenv("PROJECT_ID")
+	hostname := os.Getenv("HOST")
+	username := os.Getenv("USERNAME")
+	password := os.Getenv("PASSWORD")
+	container := os.Getenv("NAME")
+	args := os.Getenv("ARGS")
+	projectID := os.Getenv("PROJECT_ID")
 
 	//Start a Windows VM on GCE in the background if required.
-	if (host == "") || (username == "") || (password == "") {
-		log.Print("Starting Windows VM")
-		svc, err := builder.GCEService(ctx)
-		if err != nil {
-			log.Fatalf("Failed to start GCE service: %v", err)
-		}
-		inst, err = builder.StartWindowsVM(ctx, svc, projectID)
-		if err != nil {
-			log.Fatalf("Failed to start Windows VM: %v", err)
-		}
-		//TODO: set host, username
-		password, err = builder.ResetWindowsPassword(projectID, svc, inst, username)
-
-		//Set firewall rule.
-		err = builder.SetFirewallRule(ctx, svc, projectID)
-		if err != nil {
-			log.Fatalf("Failed to set ingress firewall rule: %v", err)
+	if (hostname == "") || (username == "") || (password == "") {
+		server := builder.NewServer(ctx, projectID)
+	} else {
+		server := builder.Server{
+			Hostname: hostname,
+			Username: username,
+			Password: password,
 		}
 	}
 
